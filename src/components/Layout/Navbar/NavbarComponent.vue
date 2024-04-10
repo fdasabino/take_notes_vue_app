013a2f620ec350bd4445aa253462d6a85476ecca
Francisco Sabino <fdasabino@gmail.com>
2024-04-10 22:27:19 +0200

    Refactor NavbarComponent styles and update navigation links

--- src/components/Layout/Navbar/NavbarComponent.vue
+++ src/components/Layout/Navbar/NavbarComponent.vue
@@ -3,21 +3,29 @@
 import { BookOpenIcon, PencilIcon, PresentationChartBarIcon } from "@heroicons/vue/24/outline";
 import { RouterLink } from "vue-router";
 import "./NavbarComponent.scss";
-
-// logic
 </script>
 
 <template>
     <div class="navbar">
-        <div class="navbar__left">
-            <RouterLink to="/">
-                <PencilIcon />
-                <h1>Notes App</h1>
-            </RouterLink>
-        </div>
-        <div class="navbar__right">
-            <RouterLink to="/"><BookOpenIcon /> Notes</RouterLink>
-            <RouterLink to="/stats"><PresentationChartBarIcon /> Stats</RouterLink>
+        <div class="wrapper">
+            <div class="wrapper__left">
+                <RouterLink to="/">
+                    <PencilIcon />
+                    <h1>Take Notes</h1>
+                </RouterLink>
+            </div>
+            <div class="wrapper__right">
+                <RouterLink
+                    to="/"
+                    active-class="active"
+                    ><BookOpenIcon /> Notes</RouterLink
+                >
+                <RouterLink
+                    to="/stats"
+                    active-class="active"
+                    ><PresentationChartBarIcon /> Stats</RouterLink
+                >
+            </div>
         </div>
     </div>
 </template>
