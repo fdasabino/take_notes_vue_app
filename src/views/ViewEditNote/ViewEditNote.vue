0820a86864c1a870449faf760a1582cefce3e7d9
Francisco Sabino <fdasabino@gmail.com>
2024-04-15 09:56:26 +0200

    Update styles in NoteCardComponent.scss and ViewEditNote.scss

--- src/views/ViewEditNote/ViewEditNote.vue
+++ src/views/ViewEditNote/ViewEditNote.vue
@@ -2,22 +2,20 @@
 import ButtonComponent from "@/components/Layout/Button/ButtonComponent.vue";
 import CustomInputComponent from "@/components/Layout/CustomInput/CustomInputComponent.vue";
 import { useStoreNotes } from "@/stores/storeNotes";
-import { BackwardIcon, FolderIcon, XCircleIcon } from "@heroicons/vue/24/outline";
+import { BackwardIcon, FolderIcon } from "@heroicons/vue/24/outline";
 import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import "./ViewEditNote.scss";
 
 const { getNoteById, updateNote } = useStoreNotes();
-
 const route = useRoute();
+const router = useRouter();
 
 const note = computed(() => {
     return getNoteById(route.params.id as string);
 });
 
 const noteContent = ref(note.value?.content || "");
-
-const router = useRouter();
 
 const sendMeBack = () => {
     router.back();
@@ -30,8 +28,10 @@ const saveNote = () => {
 </script>
 
 <template>
-    <div class="editNote">
-        <h3>Edit Note</h3>
+    <div class="edit__note">
+        <div class="edit__note__header">
+            <h3>Edit Note</h3>
+        </div>
         <CustomInputComponent
             v-model="noteContent"
             placeholder="Edit note...">
