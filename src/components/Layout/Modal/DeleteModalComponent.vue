08f8082203d2ee3d82baadc0ba608f1eda7a1ee9
Francisco Sabino <fdasabino@gmail.com>
2024-04-17 00:12:18 +0200

    Update dependencies to include @vueuse/components

--- src/components/Layout/Modal/DeleteModalComponent.vue
+++ src/components/Layout/Modal/DeleteModalComponent.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { vOnClickOutside, vOnKeyStroke } from "@vueuse/components";
+import { onKeyStroke } from "@vueuse/core";
 import { defineProps } from "vue";
 import ButtonComponent from "../Button/ButtonComponent.vue";
 import "./DeleteModal.scss";
@@ -26,13 +28,20 @@ const handleDelete = () => {
     props.deleteFunction(props.id);
     props.closeModal();
 };
+
+onKeyStroke("Escape", () => {
+    props.closeModal();
+});
 </script>
 
 <template>
     <div
         v-if="props.showModal"
+        ref="modal"
         class="modal">
-        <div class="modal__content">
+        <div
+            class="modal__content"
+            v-on-click-outside="props.closeModal as any">
             <div class="modal__content__header">
                 <h2>Delete Note</h2>
                 <p>Are you sure you want to delete this note?</p>
