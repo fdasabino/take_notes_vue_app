c72b7b6e1c0632792aca3d3f12292fe47c4eb31f
Francisco Sabino <fdasabino@gmail.com>
2024-04-17 13:08:49 +0200

    Update .gitignore and firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,15 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
+
+// import environment variables
+const firebase_api_key = import.meta.env.VITE_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const senderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_FIREBASE_APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAhbdQkvk1kzJM5w1Ody0Pkp8Ew01gyqJk",
-    authDomain: "take-notes-46820.firebaseapp.com",
-    projectId: "take-notes-46820",
-    storageBucket: "take-notes-46820.appspot.com",
-    messagingSenderId: "804717540080",
-    appId: "1:804717540080:web:5fc6cc7aef1b6011004835",
+    apiKey: firebase_api_key,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: senderId,
+    appId: appId,
 };
 
 // Initialize Firebase
